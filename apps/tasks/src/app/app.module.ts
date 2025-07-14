92a3e5a5b62b3ae15bb3a3c705633b58aa5ce6a1
jowjow22 <jonathan224santos@gmail.com>
2025-07-14 18:16:28 -0300

    fix: service import

--- apps/tasks/src/app/app.module.ts
+++ apps/tasks/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ProjectEventsConsumer } from '../consumer/consumer.controller';
 import { TasksModule } from '../tasks/tasks.module';
 import { AuthModule } from '../auth/auth.module';
 import { TasksService } from '../tasks/tasks.service';
 import { PrismaService } from '../common/prisma.service';
+import {  ProjectEventsConsumer } from '../consumer/consumer.controller';
+import { ConsumerService } from '../consumer/consumer.service';
 
 @Module({
   imports: [AuthModule, TasksModule],
   controllers: [AppController, ProjectEventsConsumer],
-  providers: [TasksService, PrismaService],
+  providers: [TasksService, PrismaService, ConsumerService],
 })
 export class AppModule {}
