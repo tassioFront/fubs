26a672062a1fe1c65265eae121d7e5db26aee8fa
tassioFront <tassio72@hotmail.com>
2025-08-23 15:40:36 -0300

    feat(gary): change path app

--- apps/gary/jest.config.ts
+++ apps/gary/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'gary',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/gary',
+  coverageDirectory: '../../coverage/gary',
 };
