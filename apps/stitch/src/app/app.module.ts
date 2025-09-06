af3bb7934647c68f69b8462ce484d7b91684e8b6
tassioFront <tassio72@hotmail.com>
2025-09-06 15:51:54 -0300

    refactor: remove checkout and orders modules, streamline application structure

--- apps/stitch/src/app/app.module.ts
+++ apps/stitch/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CheckoutModule } from '../checkout/checkout.module';
 import { WebhookModule } from '../webhook/webhook.module';
-import { OrdersModule } from '../orders/orders.module';
 import { PlansModule } from '../plans/plans.module';
-import { OutboxModule } from '../outbox/outbox.module';
+import { OutboxProcessorModule } from '../outbox/outbox.module';
 import { CustomerModule } from '../customer/customer.module';
 import { SubscriptionModule } from '../subscription/subscription.module';
 import { AuthModule } from '../common/auth/auth.module';
@@ -13,11 +11,9 @@ import { AuthModule } from '../common/auth/auth.module';
 @Module({
   imports: [
     AuthModule,
-    CheckoutModule,
     WebhookModule,
-    OrdersModule,
     PlansModule,
-    OutboxModule,
+    OutboxProcessorModule,
     CustomerModule,
     SubscriptionModule,
   ],
